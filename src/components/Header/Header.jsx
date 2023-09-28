3f34a24c67a2a70495ed269464b3646b6fca28db
grishanych <alsolo2011@gmail.com>
2023-09-28 07:05:43 +0300

    fix some styles in nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,13 +12,13 @@ function Header() {
         <div className={styles.header}>
             <Link to="/" className={styles.logo}><img src={logo} alt="" width={70} height={70} /></Link>
             <ul className={styles.navItem}>
-                <Link className={styles.navList} to="/">головна</Link>
-                <Link className={styles.navList} to="/about_us">про нас</Link>
-                <Link className={styles.navList} to="/categories">категорії</Link>
-                <Link className={styles.navList} to="/cart">корзина</Link>
-                <Link className={styles.navList} to="/report">звіти</Link>
-                <Link className={styles.navList} to="/blog">блог</Link>
-                <Link className={styles.navList} to="/contacts">контакти</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/">головна</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/about_us">про нас</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/categories">категорії</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/cart">корзина</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/report">звіти</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/blog">блог</Link>
+                <Link className={`${styles.navList} ${styles.navLink}`} to="/contacts">контакти</Link>
             </ul>
             <div className={styles.navRightSideMenu}>
                 <SearchInHeader />
