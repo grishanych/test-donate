1f747072bacbc3e4ed0a40c54e8d4d26f3aa6cbf
Oleksandra <sashkapilipon@gmail.com>
2023-09-29 16:21:53 +0300

    footer section with adaptive - DONE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header/>
-      </header>
+      <footer className="App-footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
