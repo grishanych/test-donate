84d1b31b77cbd74799cb38d342181e634d7bb263
Rostyslav <rostykfox911@gmail.com>
2023-10-02 17:01:14 +0300

    add new branch toolkit redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useSelector , useDispatch } from "react-redux";
+import { increment, decrement, reset } from '../redux/slices/counterSlice';
 import Context from "../components/Context";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
@@ -13,11 +15,14 @@ import Home from "../components/routs/Home"
 import Categorys from "../components/routs/Categories"
 
 
+
 function App() {
 
   const [isLinkVisible, setIsLinkVisible] = useState(true);
   const contextData = { isLinkVisible, setIsLinkVisible };
 
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
@@ -36,6 +41,10 @@ function App() {
             </Routes>
           </Main>
           <Footer />
+          <h1>Лічильник: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>Збільшити</button>
+      <button onClick={() => dispatch(decrement())}>Зменшити</button>
+      <button onClick={() => dispatch(reset())}>Скинути</button>
         </Context.Provider>
       </BrowserRouter>
     </div>
