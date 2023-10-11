ee493074fa1282cccc4cc37352fe6559ad8ad4d7
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2023-10-11 12:46:53 +0300

    style slider Ok

--- src/components/slider/MainSlider.jsx
+++ src/components/slider/MainSlider.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import picOne from './../../images/bet-on-victory-1.png';
 import picTwo from './../../images/bet-on-victory-2.png';
 import picThree from './../../images/bet-on-victory-3.png';
@@ -11,22 +12,29 @@ function MainSlider(){
     const timer = setInterval(() => {
       setCurrentPic(prev => (prev + 1) % 3);
     }, 5000);
-
+    
     return () => clearInterval(timer);
   }, []);
-
+  
+  // console.log(currentPic);
   const pictures = [picOne, picTwo, picThree];
 
   return (
     <section className={styles.section}>
-      {/* <Link> */}
-      {/* to - через if */}
-        <img src={pictures[currentPic]} alt="alt" />
-      {/* </Link> */}
+      <Link to={
+        currentPic === 0 ? "/product/3456726" : currentPic === 1 ? "/product/547790" : currentPic === 2 ? "/product/111644" : null
+      }
+      className={styles.mainPisture}
+      target="_blank">
+        <img src={pictures[currentPic]} alt="alt"/>
+      </Link>
+      
       <div className={styles.imageswrapper}>
-        <img src={picOne} alt="alt" onClick={() => setCurrentPic(0)} className={styles.image}/>
-        <img src={picTwo} alt="alt" onClick={() => setCurrentPic(1)} className={styles.image}/>
-        <img src={picThree} alt="alt" onClick={() => setCurrentPic(2)} className={styles.image}/>
+        <div className={styles.images}>
+          <img src={picOne} alt="alt" onClick={() => setCurrentPic(0)} className={styles.image}/>
+          <img src={picTwo} alt="alt" onClick={() => setCurrentPic(1)} className={styles.image}/>
+          <img src={picThree} alt="alt" onClick={() => setCurrentPic(2)} className={styles.image}/>
+        </div>
       </div>
     </section>
   );
