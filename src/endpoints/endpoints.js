552b868856898691d9f4e0542445521aa98283db
grishanych <alsolo2011@gmail.com>
2023-11-15 20:25:16 +0200

    test

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,7 +1,7 @@
-export const LOGIN_URL = "http://localhost:4000/api/customers/login";
-export const REGISTRATION_URL = "http://localhost:4000/api/customers";
-export const SUBSCRIBE_URL = "http://localhost:4000/api/subscribers";
-export const GET_PRODUCTS_URL = "http://localhost:4000/api/products";
-export const GET_FAVORITES = "http://localhost:4000/api/customers/customer";
-export const NEW_CART_URL = "http://localhost:4000/api/cart";
-export const MAKE_ORDERS = "http://localhost:4000/api/orders";
+export const LOGIN_URL = "https://final-donate-fe-be-jysi.vercel.app/api/customers/login";
+export const REGISTRATION_URL = "https://final-donate-fe-be-jysi.vercel.app/api/customers";
+export const SUBSCRIBE_URL = "https://final-donate-fe-be-jysi.vercel.app/api/subscribers";
+export const GET_PRODUCTS_URL = "https://final-donate-fe-be-jysi.vercel.app/api/products";
+export const GET_FAVORITES = "https://final-donate-fe-be-jysi.vercel.app/api/customers/customer";
+export const NEW_CART_URL = "https://final-donate-fe-be-jysi.vercel.app/api/cart";
+export const MAKE_ORDERS = "https://final-donate-fe-be-jysi.vercel.app/api/orders";
